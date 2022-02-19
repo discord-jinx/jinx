fd10ad1b2127568eef75c268a8455f7794eea79f
nevenxr <nevenxr@gmail.com>
2022-02-19 11:10:00 +0600

    feat(EventHandler): handle the events

---
+++ src/handlers/event/EventHandler.ts
@@ -0,0 +1,32 @@
+import { JinxClient, JinxHandler, JinxHandlerOptions } from "../..";
+import { Event } from "..";
+
+export class EventHandler extends JinxHandler {
+    constructor(client: JinxClient, {
+        directory,
+        instance = Event
+    } = {} as JinxHandlerOptions) {
+        super(client, { directory, instance });
+    };
+
+    public load(filepath: string) {
+        this.client.util.getModule(filepath, this);
+
+        return this.setup();
+    };
+
+    private setup() {
+        const events = this.client.util.getAllCollectionKeys(this.cache);
+
+        for (let i = 0; i < events.length; i++) {
+            const event = this.cache.get(events[i]) as Event;
+
+            if (event.name) {
+                this.client[event.once ? "once" : "on"](
+                    event.name,
+                    (...args) => event.run(...args)
+                );
+            };
+        };
+    };
+};
