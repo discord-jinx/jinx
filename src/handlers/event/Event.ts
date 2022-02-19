b66ea0dd58e0bd6b02f66b683f11a949a5c44e30
nevenxr <nevenxr@gmail.com>
2022-02-19 11:19:04 +0600

    feat(Event): add class for the events

---
+++ src/handlers/event/Event.ts
@@ -0,0 +1,50 @@
+import { ClientEvents } from "discord.js";
+import { EventHandler } from "..";
+import { JinxClient } from "../..";
+
+/**
+ * Represents a event
+ * @param {EventOptions} options - The options for the event
+ */
+export class Event {
+    public client!: JinxClient;
+    public name: string;
+    public once?: boolean;
+    public handler!: EventHandler;
+    public readonly id: string;
+
+    constructor (options: EventOptions) {
+        const { name, once } = options;
+        
+        /**
+         * The name of the event
+         * @type {string}
+         */
+        this.name = name;
+
+        /**
+         * Whether to once the event
+         * @type {boolean}
+         */
+        this.once = Boolean(once);
+
+        /**
+         * The id of the event
+         * @type {string}
+         */
+        this.id = "event-" + this.name;
+
+        this.handler.cache.set(this.id, this);
+    };
+    
+    public run (...args) {
+        return;
+    };
+};
+
+export type EventNames = keyof ClientEvents;
+
+export interface EventOptions {
+    name: EventNames;
+    once?: boolean;
+};
