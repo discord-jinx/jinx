a073342ddcb4d1b9408a2e81b47c9dc70dd967cf
nevenxr <nevenxr@gmail.com>
2022-02-20 21:34:23 +0600

    feat(JinxError): add "NO_DIRECTORY" error

--- src/utils/JinxError.ts
+++ src/utils/JinxError.ts
@@ -2,7 +2,8 @@ export const JinxErrorKeys: JinxErrorKeysType = {
     "NO_MODULE_NAME": "Module name cannot be empty.",
     "NOT_AN_INSTANCE": (type) => `Module isn't an instance of the ${type}.`,
     "NO_ID": (action) => `Module id is needed to ${action}.`,
-    "NO_FILE_PATH": "The file path is required to load the module."
+    "NO_FILE_PATH": "The file path is required to load the module.",
+    "NO_DIRECTORY": (module) => `The directory path to ${module} is required.`
 };
 
 export type JinxErrorKeysType = {
@@ -13,7 +14,8 @@ export type JinxErrorKeysTypeKeys =
 "NO_MODULE_NAME" |
 "NOT_AN_INSTANCE" |
 "NO_ID" |
-"NO_FILE_PATH";
+"NO_FILE_PATH" |
+"NO_DIRECTORY";
 
 export class JinxError extends Error {
     constructor (key: keyof (typeof JinxErrorKeys), ...args) {
