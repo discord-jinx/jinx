3ac958bec5a5e16fa2fb7ddac8023764fefd910a
nevenxr <nevenxr@gmail.com>
2022-02-20 21:05:12 +0600

    feat(JinxError): add more keys

--- src/utils/JinxError.ts
+++ src/utils/JinxError.ts
@@ -1,6 +1,8 @@
 export const JinxErrorKeys: JinxErrorKeysType = {
     "NO_MODULE_NAME": "Module name cannot be empty.",
-    "NOT_AN_INSTANCE": (type) => `Module isn't an instance of the ${type}.`
+    "NOT_AN_INSTANCE": (type) => `Module isn't an instance of the ${type}.`,
+    "NO_ID": (action) => `Module id is needed to ${action}.`,
+    "NO_FILE_PATH": "The file path is required to load the module."
 };
 
 export type JinxErrorKeysType = {
@@ -9,7 +11,9 @@ export type JinxErrorKeysType = {
 
 export type JinxErrorKeysTypeKeys = 
 "NO_MODULE_NAME" |
-"NOT_AN_INSTANCE";
+"NOT_AN_INSTANCE" |
+"NO_ID" |
+"NO_FILE_PATH";
 
 export class JinxError extends Error {
     constructor (key: keyof (typeof JinxErrorKeys), ...args) {
