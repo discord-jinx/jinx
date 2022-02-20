605b1e7b45ba8ec074a391e5e6d74c12260f875f
nevenxr <nevenxr@gmail.com>
2022-02-20 21:33:43 +0600

    style(index): add comments

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+// Structures
 export * from "./structs/JinxClient";
 export * from "./structs/JinxHandler";
 export * from "./structs/JinxInvite";
 export * from "./structs/JinxModule";
+
+// Utilities
 export * from "./utils/JinxError";
 export * from "./utils/Util";
 export * from "./utils/Fetch";
+
+// Handlers
 export * from "./handlers";
