6b20380cc86f83d90656b38be36285db8cc73ac9
nevenxr <nevenxr@gmail.com>
2022-02-21 12:27:16 +0600

    feat(JinxError): add missing intent error

--- src/utils/JinxError.ts
+++ src/utils/JinxError.ts
@@ -3,7 +3,8 @@ export const JinxErrorKeys: JinxErrorKeysType = {
     "NOT_AN_INSTANCE": (type) => `Module isn't an instance of the ${type}.`,
     "NO_ID": (action) => `Module id is needed to ${action}.`,
     "NO_FILE_PATH": "The file path is required to load the module.",
-    "NO_DIRECTORY": (module) => `The directory path to ${module} is required.`
+    "NO_DIRECTORY": (module) => `The directory path to ${module} is required.`,
+    "MISSING_INTENT": (intent, fr) => `${intent} intent is missing and required to ${fr}`
 };
 
 export type JinxErrorKeysType = {
@@ -15,7 +16,8 @@ export type JinxErrorKeysTypeKeys =
 "NOT_AN_INSTANCE" |
 "NO_ID" |
 "NO_FILE_PATH" |
-"NO_DIRECTORY";
+"NO_DIRECTORY" |
+"MISSING_INTENT";
 
 export class JinxError extends Error {
     constructor (key: keyof (typeof JinxErrorKeys), ...args) {
