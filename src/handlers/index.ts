0e2ccad38a5f2c44d1fa66cba7f5caf00c5789aa
nevenxr <nevenxr@gmail.com>
2022-02-25 21:32:15 +0600

    feat(handler): export Command & CommandHandler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -5,3 +5,7 @@ export * from "./application/ApplicationHandler";
 // Event
 export * from "./event/Event";
 export * from "./event/EventHandler";
+
+// Command
+export * from "./command/Command";
+export * from "./command/CommandHandler";
