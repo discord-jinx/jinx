023977e9dd7150cd87efafb7c4f3596405980bfe
nevenxr <nevenxr@gmail.com>
2022-02-25 21:31:36 +0600

    refactor(index): export JinxCooldown

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./structs/JinxInvite";
 export * from "./structs/JinxModule";
 
 // Utilities
+export * from "./utils/JinxCooldown";
 export * from "./utils/JinxError";
 export * from "./utils/Util";
 export * from "./utils/Fetch";
